32f7662c93a673e2410168cc6f19b7d3b9a6b2f6
Rémi Campistron <remi.campistron@gmail.com>
2026-03-25 15:20:29 +0100

    Initial commit

---
+++ src/test/java/fr/fms/booking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.fms.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
